d0512d3a51a1b59a61b1dec0c172600a0cdec86f
Gregor Weber <gweber@mozilla.com>
2020-04-24 23:06:58 +0200

    rewrite bcd table

--- client/src/document/index.tsx
+++ client/src/document/index.tsx
@@ -266,7 +266,7 @@ function RenderDocumentBody({ doc }) {
       return (
         <BrowserCompatibilityTable
           key={`browser_compatibility${i}`}
-          data={section.value}
+          {...section.value}
         />
       );
     } else if (section.type === "examples") {
--- client/src/document/ingredients/browser-compatibility-table/legend.tsx
+++ client/src/document/ingredients/browser-compatibility-table/legend.tsx
@@ -1,267 +1,109 @@
 import React from "react";
+import type bcd from "mdn-browser-compat-data/types";
+import { asList, listFeatures } from "./utils";
 
-// function buildIndexNotes(
-//   browserSupportDetails,
-//   rowIndex,
-//   currentNoteId,
-//   hasFlag,
-//   hasPrefix,
-//   hasAlternative,
-//   hasNotes
-// ) {
-//   const indexNotes = browserSupportDetails.map(
-//     ({ browser, support, version_added }, detailIndex) => {
-//       if (Array.isArray(support)) {
-//         const [notes, flags, prefixes, alternatives] = [[], [], [], []];
-//
-//         for (const supportItem of support) {
-//           if (!!supportItem.alternative_name) {
-//             hasAlternative = true;
-//             alternatives.push(supportItem);
-//           } else if (!!supportItem.prefix) {
-//             hasPrefix = true;
-//             prefixes.push(supportItem);
-//           } else if (Array.isArray(supportItem.flags)) {
-//             hasFlag = true;
-//             flags.concat(supportItem.flags);
-//           } else if (!!supportItem.notes) {
-//             hasNotes = true;
-//             if (Array.isArray(supportItem.notes)) {
-//               notes.concat(supportItem.notes);
-//             } else {
-//               notes.push(supportItem.notes);
-//             }
-//           }
-//         }
-//
-//         return {
-//           index: `${rowIndex}-${detailIndex}`,
-//           browser,
-//           version_added,
-//           support,
-//           prefixes,
-//           alternatives,
-//           flags,
-//           notes,
-//         };
-//       } else {
-//         if (!hasFlag) {
-//           hasFlag = !!(support && support.flags);
-//         }
-//         if (!hasPrefix) {
-//           hasPrefix = !!(support && support.prefix);
-//         }
-//         if (!hasNotes) {
-//           hasNotes = !!(support && support.notes);
-//         }
-//
-//         const prefixes = !!(support && support.prefix) ? [support] : [];
-//         const alternatives = !!(support && support.alternative_name)
-//           ? [support]
-//           : [];
-//         const flags = !!(support && support.flags) ? support.flags : [];
-//         const notes = gatherNotesForIndexNote(support);
-//
-//         return {
-//           index: `${rowIndex}-${detailIndex}`,
-//           browser,
-//           version_added,
-//           support,
-//           prefixes,
-//           alternatives,
-//           flags,
-//           notes,
-//         };
-//       }
-//     }
-//   );
-//
-//   const filteredIndexNotes = indexNotes.filter(
-//     (indexNotes) => `bc-history-${indexNotes.index}` === currentNoteId
-//   );
-//
-//   return [filteredIndexNotes, hasFlag, hasPrefix, hasAlternative, hasNotes];
-// }
+// Also specifies the order in which the legend appears
+const LEGEND_LABELS = {
+  yes: "Full support",
+  partial: "Partial support",
+  no: "No support",
+  experimental: "Experimental. Expect behavior to change in the future.",
+  "non-standard": "Non-standard. Expect poor cross-browser support.",
+  deprecated: "Deprecated. Not for use in new websites.",
+  footnote: "See implementation notes.",
+  disabled: "User must explicitly enable this feature.",
+  altname: "Uses a non-standard name.",
+  prefix: "Requires a vendor prefix or different name for use.",
+};
+type LEGEND_KEY = keyof typeof LEGEND_LABELS;
 
-export function Legend({ compatibilityData }) {
-  let [
-    hasDeprecation,
-    hasExperimental,
-    hasNonStandard,
-    hasFlag,
-    hasPrefix,
-    hasAlternative,
-    hasNotes,
-  ] = [false, false, false, false, false, false, false];
-  // const compatibility = buildCompatibilityObject(
-  //   compatibilityData.query,
-  //   compatibilityData.data
-  // );
-  // for (const key in compatibility) {
-  //   const currentRow = compatibility[key];
-  //
-  //   if (currentRow.status) {
-  //     if (!hasDeprecation) {
-  //       hasDeprecation = !!currentRow.status.deprecated;
-  //     }
-  //     if (!hasExperimental) {
-  //       hasExperimental = !!currentRow.status.experimental;
-  //     }
-  //     if (!hasNonStandard) {
-  //       hasNonStandard = !!currentRow.status.standard_track;
-  //     }
-  //   }
-  //
-  //   const browserSupportDetails = displayBrowsers.map((browser) => {
-  //     const support = currentRow.support[browser];
-  //     const version_added = getVersionAdded(support);
-  //     return { browser, support, version_added };
-  //   });
-  //
-  //   [
-  //     hasFlag,
-  //     hasPrefix,
-  //     hasAlternative,
-  //     hasNotes,
-  //   ] = buildIndexNotes(
-  //     browserSupportDetails,
-  //     key,
-  //     currentNoteId,
-  //     hasFlag,
-  //     hasPrefix,
-  //     hasAlternative,
-  //     hasNotes
-  //   );
-  // }
+function getActiveLegendItems(compat: bcd.Identifier) {
+  const legendItems = new Set<LEGEND_KEY>();
+
+  for (const feature of listFeatures(compat)) {
+    const { status } = feature.compat;
+
+    if (status) {
+      if (status.experimental) {
+        legendItems.add("experimental");
+      }
+      if (status.deprecated) {
+        legendItems.add("deprecated");
+      }
+      if (!status.standard_track) {
+        legendItems.add("non-standard");
+      }
+    }
+
+    for (const browserSupport of Object.values(feature.compat.support)) {
+      if (!browserSupport) {
+        legendItems.add("no");
+        continue;
+      }
+
+      for (const versionSupport of asList(browserSupport)) {
+        if (versionSupport.version_added) {
+          legendItems.add("yes");
+        } else if (versionSupport.version_added !== null) {
+          legendItems.add("no");
+        }
+
+        if (versionSupport.partial_implementation) {
+          legendItems.add("partial");
+        }
+        if (versionSupport.prefix) {
+          legendItems.add("prefix");
+        }
+        if (versionSupport.notes) {
+          legendItems.add("footnote");
+        }
+        if (versionSupport.alternative_name) {
+          legendItems.add("altname");
+        }
+        if (versionSupport.flags) {
+          legendItems.add("disabled");
+        }
+      }
+    }
+  }
+  return Object.keys(LEGEND_LABELS)
+    .filter((key) => legendItems.has(key as LEGEND_KEY))
+    .map((key) => [key, LEGEND_LABELS[key]]);
+}
+
+export function Legend({ compat }: { compat: bcd.Identifier }) {
   return (
     <section className="bc-legend">
       <h3 className="offscreen highlight-spanned" id="Legend">
         <span className="highlight-span">Legend</span>
       </h3>
       <dl>
-        <dt>
-          <span className="bc-supports-yes bc-supports">
-            <abbr
-              className="bc-level bc-level-yes only-icon"
-              title="Full support"
-            >
-              <span>Full support</span>
-            </abbr>
-          </span>
-        </dt>
-        <dd>Full support</dd>
-        <dt>
-          <span className="bc-supports-no bc-supports">
-            <abbr className="bc-level bc-level-no only-icon" title="No support">
-              <span>No support</span>
-            </abbr>
-          </span>
-        </dt>
-        <dd>No support</dd>
-        <dt>
-          <span className="bc-supports-unknown bc-supports">
-            <abbr
-              className="bc-level bc-level-unknown only-icon"
-              title="Compatibility unknown"
-            >
-              <span>Compatibility unknown</span>
-            </abbr>
-          </span>
-        </dt>
-        <dd>Compatibility unknown</dd>
-        {hasNotes && (
-          <>
-            <dt>
-              <abbr className="only-icon" title="See implementation notes.">
-                <span>See implementation notes.</span>
-                <i className="ic-footnote" />
-              </abbr>
-            </dt>
-            <dd>See implementation notes.</dd>
-          </>
-        )}
-        {hasDeprecation && (
-          <>
-            <dt>
-              <abbr
-                className="only-icon"
-                title="Deprecated. Not for use in new websites."
-              >
-                <span>Deprecated. Not for use in new websites.</span>
-                <i className="ic-deprecated" />
-              </abbr>
-            </dt>
-            <dd>Deprecated. Not for use in new websites.</dd>
-          </>
-        )}
-        {hasExperimental && (
-          <>
-            <dt>
-              <abbr
-                className="only-icon"
-                title="Experimental. Expect behavior to change in the future."
-              >
-                <span>
-                  Experimental. Expect behavior to change in the future.
+        {getActiveLegendItems(compat).map(([key, label]) =>
+          ["yes", "partial", "no"].includes(key) ? (
+            <React.Fragment key={key}>
+              <dt key={key}>
+                <span className={`bc-supports-${key} bc-supports`}>
+                  <abbr
+                    className={`bc-level bc-level-${key} only-icon`}
+                    title={label}
+                  >
+                    <span>{label}</span>
+                  </abbr>
                 </span>
-                <i className="ic-experimental" />
-              </abbr>
-            </dt>
-            <dd>Experimental. Expect behavior to change in the future.</dd>
-          </>
-        )}
-        {hasNonStandard && (
-          <>
-            <dt>
-              <abbr
-                className="only-icon"
-                title="Non-standard. Expect poor cross-browser support."
-              >
-                <span>Non-standard. Expect poor cross-browser support.</span>
-                <i className="ic-non-standard" />
-              </abbr>
-            </dt>
-            <dd>Non-standard. Expect poor cross-browser support.</dd>
-          </>
-        )}
-        {hasFlag && (
-          <>
-            <dt>
-              <abbr
-                className="only-icon"
-                title="User must explicitly enable this feature."
-              >
-                <span>User must explicitly enable this feature.</span>
-                <i className="ic-disabled" />
-              </abbr>
-            </dt>
-            ,<dd>User must explicitly enable this feature.</dd>
-          </>
-        )}
-        {hasPrefix && (
-          <>
-            <dt>
-              <abbr
-                className="only-icon"
-                title="Requires a vendor prefix or different name for use."
-              >
-                <span>Requires a vendor prefix or different name for use.</span>
-                <i className="ic-prefix" />
-              </abbr>
-            </dt>
-            <dd>Requires a vendor prefix or different name for use.</dd>
-          </>
-        )}
-        {hasAlternative && (
-          <>
-            <dt>
-              <abbr className="only-icon" title="Uses a non-standard name.">
-                <span>Uses a non-standard name.</span>
-                <i className="ic-altname" />
-              </abbr>
-            </dt>
-            <dd>Uses a non-standard name.</dd>
-          </>
+              </dt>
+              <dd>{label}</dd>
+            </React.Fragment>
+          ) : (
+            <React.Fragment key={key}>
+              <dt>
+                <abbr className="only-icon" title={label}>
+                  <span>{label}</span>
+                  <i className={`ic-${key}`} />
+                </abbr>
+              </dt>
+              <dd>{label}</dd>
+            </React.Fragment>
+          )
         )}
       </dl>
     </section>
